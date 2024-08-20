fc1dcc3f5bdb8db939ceae3c7260cd33df9d59c3
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-20 16:38:46 +0100

    "Removed async keyword from measure_time function"

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """returns the execution time of a function"""
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
