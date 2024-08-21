11cf419eb015a5ad22e8bb3d4b29cc373e7df436
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-21 13:04:32 +0100

    "Updated async_generator function to return AsyncGenerator[float, any] instead of any"

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """ A coroutine called async_generator that takes no arguments."""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator() -> any:
+async def async_generator() -> AsyncGenerator[float, any]:
     """ A coroutine that yields 10 numbers."""
     for i in range(10):
         yield random.uniform(0, 10)
