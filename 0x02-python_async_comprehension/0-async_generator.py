8a2d488303f74bfc6398258b56cc40973f6eccc0
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-21 12:51:06 +0100

    created python files

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" A coroutine called async_generator that takes no arguments."""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """ A coroutine that yields 10 numbers."""
+    for i in range(10):
+        yield random.uniform(0, 10)
+        await asyncio.sleep(1)
