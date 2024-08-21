e6493032e2ef79eedad74637693d9064cd90155b
Ogbonna George Ekwueme <georgekwm1@gmail.com>
2024-08-21 13:18:17 +0100

    "Changed type hint for async_generator function from AsyncGenerator to Generator"

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 """ A coroutine called async_generator that takes no arguments."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ A coroutine that yields 10 numbers."""
     for i in range(10):
         yield random.uniform(0, 10)
